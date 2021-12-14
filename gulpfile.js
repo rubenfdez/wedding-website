eca7e78fd21d54c9ed2f01a6750e1d3ed8db702b
Diego Álvarez <diegoalvarezb@gmail.com>
2021-12-14 18:28:20 +0100

    Update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-dart-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
